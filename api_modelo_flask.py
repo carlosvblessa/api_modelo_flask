a9171a22271855f5fe4abf2f4f2a9949d091e04a
carloslessa <carloslessa@sefaz.al.gov.br>
2025-04-26 12:36:37 -0300

    Corrigido requirements e api

--- api_modelo_flask.py
+++ api_modelo_flask.py
@@ -166,7 +166,3 @@ def list_predictions():
         })
     return jsonify(results)
 
-# Executar aplicação
-if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 10000))
-    app.run(host="0.0.0.0", port=port, debug=True)
