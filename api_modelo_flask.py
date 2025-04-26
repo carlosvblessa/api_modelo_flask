a44d0e44238e59817a6375d5e270c236d2b2083e
carloslessa <carloslessa@sefaz.al.gov.br>
2025-04-26 09:13:45 -0300

    Correções em host e port

--- api_modelo_flask.py
+++ api_modelo_flask.py
@@ -166,5 +166,7 @@ def list_predictions():
         })
     return jsonify(results)
 
+# Executar aplicação
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port, debug=True)
